79e0605aebc1baee8418188e33c14b233008c33f
Nishant-Ingle <30694286+Nishant-Ingle@users.noreply.github.com>
2018-06-26 15:10:34 +0530

    Update array.js

--- array.js
+++ array.js
@@ -2,5 +2,5 @@ const super_heroes = [ 'Iron Man', 'Spiderman', 'Capt. America' ]
 super_heroes[0] = true
 console.log(super_heroes[0])
 console.log(super_heroes.length)
-
+//comment
 console.log(`we have ${super_heroes.length} heroes`)
